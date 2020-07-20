74327a99fa53d4cb1f520ff83829a9d15cc5727e
Alvyn Abranches <alvynabranches@gmail.com>
2020-07-20 20:12:59 +0530

    Initial Commit

---
+++ SocketProgram/app/src/main/java/com/example/socketprogram/SendMessage.java
@@ -0,0 +1,27 @@
+package com.example.socketprogram;
+
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SendMessage extends AsyncTask<String, Void, Void> {
+    private Exception exception;
+    protected Void doInBackground(String... params){
+        try {
+            try {
+                Socket socket = new Socket("192.168.43.118", 8888);
+                PrintWriter outToServer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+                outToServer.print(params[0]);
+                outToServer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            this.exception = e;
+            return null;
+        }
+        return null;
+    }
+}
